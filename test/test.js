2734b9fecd8818ab48b02097c9e647acbc29af8a
Michaël Zasso <targos@protonmail.com>
2016-04-25 14:02:54 +0200

    fix test

--- test/test.js
+++ test/test.js
@@ -7,10 +7,12 @@ const files = ['grey8.tif', 'grey16.tif'];
 const dir = __dirname + '/img/';
 
 describe('TIFF decoder', function () {
-    for (var i = 0; i < files.length; i++) {
-        const file = fs.readFileSync(dir + files[i]);
-        const decoder = new TIFFDecoder(file);
-        const result = decoder.decode();
-        result.ifd.length.should.equal(1);
-    }
+    it('should decode', function () {
+        for (var i = 0; i < files.length; i++) {
+            const file = fs.readFileSync(dir + files[i]);
+            const decoder = new TIFFDecoder(file);
+            const result = decoder.decode();
+            result.ifd.length.should.equal(1);
+        }
+    });
 });
