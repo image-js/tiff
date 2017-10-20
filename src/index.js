ce3f8d57d6973b817e05beefc509dff97e13ae2f
Mikael Lirbank <mikael@airlab.io>
2017-10-19 22:53:27 -0700

    Add pageCount and isMultiPage props

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ function decodeTIFF(data, options = {}) {
     return decoder.decode(options);
 }
 
+function checkMultiPage(data) {
+    const decoder = new TIFFDecoder(data);
+    return decoder.isMultiPage;
+}
+
+function countPages(data) {
+    const decoder = new TIFFDecoder(data);
+    return decoder.pageCount;
+}
+
 export {
-    decodeTIFF as decode
+    decodeTIFF as decode,
+    checkMultiPage,
+    countPages,
 };
