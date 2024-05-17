6d925ebc2cbea63fa15d4af7c84e86d956b2d040
moonayyur <jm_kias@esi.dz>
2024-05-17 12:57:44 +0200

    feat: export list of tag ids

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { tagsById as exif } from './tags/exif';
+import { tagsById as gps } from './tags/gps';
+import { tagsById as standard } from './tags/standard';
 import TIFFDecoder from './tiffDecoder';
 import TiffIfd from './tiffIfd';
 import { BufferType, DecodeOptions } from './types';
@@ -17,4 +20,17 @@ function pageCount(data: BufferType): number {
   return decoder.pageCount;
 }
 
-export { decodeTIFF as decode, isMultiPage, pageCount, DecodeOptions, TiffIfd };
+const tagList = {
+  exif,
+  gps,
+  standard,
+};
+
+export {
+  decodeTIFF as decode,
+  isMultiPage,
+  pageCount,
+  DecodeOptions,
+  TiffIfd,
+  tagList,
+};
