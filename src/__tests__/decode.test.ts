4efd252f2ced961bb04ca9316147309d13b6431a
EscapedGibbon <maximgolubev2@gmail.com>
2025-07-18 00:41:25 +0200

    test: add testing case for dog.tiff

--- src/__tests__/decode.test.ts
+++ src/__tests__/decode.test.ts
@@ -190,7 +190,6 @@ const files: TiffFile[] = [
     components: 1,
     alpha: false,
   },
-
   {
     name: 'bw1bit.tif',
     width: 2,
@@ -207,6 +206,15 @@ const files: TiffFile[] = [
     components: 1,
     alpha: false,
   },
+  // Checks only the first frame of the image.
+  {
+    name: 'dog.tiff',
+    width: 16,
+    height: 16,
+    bitsPerSample: 1,
+    components: 4,
+    alpha: true,
+  },
 ];
 const cases = files.map(
   (file) => [file.name, file, readImage(file.name)] as const,
@@ -219,7 +227,7 @@ test.each(cases)(
   { timeout: 30_000 },
   (name, file, image) => {
     const result = decode(image);
-    expect(result).toHaveLength(1);
+    expect(result).toHaveLength(name === 'dog.tiff' ? 8 : 1);
     const { data, bitsPerSample, width, height, components, alpha } = result[0];
     expect(width).toBe(file.width);
     expect(height).toBe(file.height);
@@ -426,4 +434,36 @@ test('should decode 15x15 image with tile data', () => {
       ].flat(),
     ),
   );
+});
+
+test('should decode multiframe image', () => {
+  const decoded = decode(readImage('dog.tiff'));
+  expect(decoded).toHaveLength(8);
+  expect(decoded[0]).toMatchObject({
+    alpha: true,
+    bitsPerSample: 1,
+    components: 4,
+    compression: 1,
+    width: 16,
+    height: 16,
+  });
+
+  expect(decoded[0].imageWidth).toEqual(16);
+  // last row of the first frame
+  const firstFrameLastRow = new Uint8Array([
+    1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1,
+    1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1,
+    0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0,
+  ]);
+  expect(decoded[1].imageLength).toEqual(16);
+  expect(decoded[1].imageWidth).toEqual(16);
+
+  expect(decoded[0].data.slice(960, 1024)).toEqual(firstFrameLastRow);
+  // twelveth row of the second frame
+  const secondFrameTwelvethRow = new Uint8Array([
+    1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1,
+    0, 1, 0, 1, 0, 1, 0,
+  ]);
+  expect(decoded[1].samplesPerPixel).toEqual(2);
+  expect(decoded[1].data.slice(352, 384)).toEqual(secondFrameTwelvethRow);
 });
